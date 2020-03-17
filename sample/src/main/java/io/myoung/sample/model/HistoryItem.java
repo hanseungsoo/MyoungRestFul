89bc65351d8a41e4bc5407cb6e573dd3627ca46b
SoHyun <SoHyun@SoHyun-PC>
2020-03-17 22:36:31 +0900

    ff

---
+++ sample/src/main/java/io/myoung/sample/model/HistoryItem.java
@@ -0,0 +1,22 @@
+package io.myoung.sample.model;
+
+import java.util.Date;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * HistoryItem.java
+ * @클래스설명 : 변경 이력 위한 ITEM 클래스 
+ */
+@Data
+@Builder
+public class HistoryItem {
+	 
+	private int hSeq;
+	private String flag;
+	private Date actionTime;
+	private int uSeq;
+	private int fSeq;
+	private int gSeq;
+}
